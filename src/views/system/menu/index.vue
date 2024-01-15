c00990309136242718148fb60b015a1748329652
MODI47 <mhmed734876679@gmail.com>
2024-01-15 18:55:37 +0800

    updates for mobile model

--- src/views/system/menu/index.vue
+++ src/views/system/menu/index.vue
@@ -1,4 +1,5 @@
 <template>
+  <!-- <script src="//unpkg.com/@element-plus/icons-vue"></script> -->
   <div class="app-container">
     <div class="flex" v-if="showSearch">
       <search_control ref="form" :displaySearch="true" :fields="searchFields" :queryParams="queryParams"
@@ -11,55 +12,160 @@
 
     <!-- Table Header -->
     <tableHeader :isDark="isDark" buttonColor="#626aef" deleteButtonColor="red" :selectedRows="selectedRows"
-            :buttons="{ new: true, edit: true, expand: false, delete: true, filter: true }" :handleAdd="handleAdd"
-            :handleUpdate="handleUpdate" :toggleExpandAll="toggleExpandAll" :handleDelete="handleDelete"
-            :showSearch="showSearch" @toggleFilter="showSearch = !showSearch"
-            :permissions="{ new: 'system:user:add', edit: 'system:user:edit', delete: 'system:post:remove' }" />
+      :buttons="{ new: true, edit: true, expand: false, delete: true, filter: true }" :handleAdd="handleAdd"
+      :handleUpdate="handleUpdate" :toggleExpandAll="toggleExpandAll" :handleDelete="handleDelete"
+      :showSearch="showSearch" @toggleFilter="showSearch = !showSearch"
+      :permissions="{ new: 'system:user:add', edit: 'system:user:edit', delete: 'system:post:remove' }" />
 
 
     <!-- Table view  -->
     <el-config-provider>
-      <el-table flex :data="menuList" style="width:100%" row-key="parentId" v-loading="loading"
-        element-loading-text="Loading..." 
-        :default-expand-all="isExpandAll"  v-if="refreshTable"
-        @selection-change="handleSelectionChange">
-
-        <el-table-column :selectable="selectable" type="selection" ></el-table-column>
-        <el-table-column fixed prop="name" label="Name"  />
-        <el-table-column fixed prop="type" label="Type"  />
-        <el-table-column prop="icon" label="Icon" align="center" >
-          <template slot-scope="scope">
-            <!-- <svg-icon :icon-class="scope.row.icon" /> -->
-          </template>
+      <el-table flex :data="menuList" style="width:100%" row-key="menuId" v-loading="loading"
+        element-loading-text="Loading..." :default-expand-all="isExpandAll" v-if="refreshTable"
+        @selection-change="handleSelectionChange" show-overflow-tooltip="true">
+        <el-table-column :selectable="selectable" type="selection"></el-table-column>
+        <el-table-column label="Name" prop="name" class="no-wrap">
+          <!-- <template #default="{ row }">
+            <div class="no-wrap">{{ row.name }}</div>
+          </template> -->
         </el-table-column>
-        <el-table-column fixed prop="orderNum" label="Order"  />
-        <el-table-column fixed prop="status" label="Status" >
-          <template #default="{ row }">
-            <el-tag :type="row.status === 0 ? 'success' : 'danger'">
-              {{ row.status === 0 ? 'Enabled' : 'Disabled' }}
-            </el-tag>
-          </template>
-        </el-table-column>
-        <el-table-column prop="component" label="Component"  />
-        <el-table-column prop="path" label="Path"  />
-        <!-- <el-table-column type="icon" prop="Icon " label="Icons" width="200" /> -->
-        <el-table-column prop="remark" label="Note"  />
-        <el-table-column prop="createBy" label="ADD By "  />
-        <el-table-column fixed="right" label="Actions"  align="center" class-name="small-padding fixed-width">
+        <el-table-column fixed="right" label="Actions" align="center" class-name="small-padding fixed-width">
           <template #default="{ row, column, index }">
-            <el-row class="mb-4">
-              <el-button size="mini" type="text" @click="handleAdd(row, index)" :el-icon-plus="Add"
-                v-hasPermi="['system:user:add']" v-if="row.type === 0">
-                Add</el-button>
-              <el-button type="primary" :el-icon-plus="Edit" size="small" @click="handleUpdate(row)"
-                v-hasPermi="['system:user:edit']">
-                Edit</el-button>
-              <el-button type="warning" :el-icon-plus="Delete" size="small" @click="handle_SideDelete(row)"
-                v-hasPermi="['system:user:remove']">Delete</el-button>
-            </el-row>
+            <div class="actions_buttons">
+              <el-icon :size="15" color="lightblue" size="mini" type="text" @click="handleAdd(row, index)"
+                :el-icon-plus="Add" v-hasPermi="['system:user:add']" v-if="row.type === 0">
+                <Plus />
+              </el-icon>
+              <el-icon :size="15" color="blue" @click="handleUpdate(row)" v-hasPermi="['system:user:edit']">
+                <Edit />
+              </el-icon>
+              <el-icon :size="15" color="red" :el-icon-plus="Delete" size="small" @click="handle_SideDelete(row)"
+                v-hasPermi="['system:user:remove']">
+                <Delete />
+              </el-icon>
+              <el-icon :size="15" color="silver" @click="openDetails(row)">
+                <View />
+              </el-icon>
+            </div>
+          </template>
+        </el-table-column>
+        <el-table-column >
+          <template #default="{row}">
+            <el-dialog class="inside-dialog" v-model="detailsDialogVisible" title="Details">
+              <div class="button-container">
+                <el-icon :size="20" color="green" size="mini" @click="handleAdd(row)" :el-icon-plus="Add"
+                  v-hasPermi="['system:user:add']" v-if="row.type === 0">
+                  <Plus />
+                </el-icon>
+                <el-icon :size="20" color="blue" @click="handleUpdate(row)" v-hasPermi="['system:user:edit']">
+                  <Edit />
+                </el-icon>
+                <el-icon :size="20" color="red" :el-icon-plus="Delete" @click="handle_SideDelete(row)"
+                  v-hasPermi="['system:user:remove']">
+                  <Delete />
+                </el-icon>
+              </div>
+              <div v-if="mobileView" class="details-container">
+                <div class="details-row">
+                  <div class="details-label">Name:</div>
+                  <div class="details-value">{{ row.name }}</div>
+                </div>
+                <div class="details-row">
+                  <div class="details-label">Note:</div>
+                  <div class="details-value">{{ row.remark }}</div>
+                </div>
+                <div class="details-row">
+                  <div class="details-label">Type:</div>
+                  <div class="details-value">
+                    <el-tag :type="row.type === 0 ? 'success' : row.type === 1 ? 'Silver' : 'warning'">
+                      {{ row.type === 0 ? 'Directory' : row.type === 1 ? 'File' : 'Link' }}
+                    </el-tag>
+                  </div>
+                </div>
+                <div class="details-row">
+                  <div class="details-label">status:</div>
+                  <div class="details-value">
+                    <el-tag :type="row.status === 0 ? 'success' : row.status === 1 ? 'danger' : 'warning'">
+                      {{ row.status === 0 ? 'Enabled' : row.status === 1 ? 'Disabled' : 'Sleep' }}
+                    </el-tag>
+                  </div>
+                </div>
+              </div>
+            </el-dialog>
           </template>
         </el-table-column>
       </el-table>
+
+      <!-- <el-dialog width="100%"  v-model="detailsDialogVisible" title="Details">
+        <div class="button-container">
+          <el-icon :size="20" color="green" size="mini"  @click="handleAdd(mobileView)" :el-icon-plus="Add"
+            v-hasPermi="['system:user:add']" v-if="mobileView.type === 0">
+            <Plus />
+          </el-icon>
+          <el-icon :size="20" color="blue" @click="handleUpdate(mobileView)" v-hasPermi="['system:user:edit']">
+            <Edit />
+          </el-icon>
+          <el-icon :size="20" color="red" :el-icon-plus="Delete"  @click="handle_SideDelete(mobileView)"
+            v-hasPermi="['system:user:remove']">
+            <Delete />
+          </el-icon>
+        </div>
+        <div v-for="item in items" :key="item.key" class="tableRow">
+          <div class="tableCell tableHeader">{{ item.label }}</div>
+          <div class="tableCell tableData">{{ mobileView[item.key] }}</div>
+        </div>
+      </el-dialog> -->
+
+
+      <!-- <el-dialog v-model="detailsDialogVisible" title="Row Details">
+        <el-table :data="mobileView ? [mobileView] : []">
+          <el-table-column prop="name" label="Name"></el-table-column>
+          <el-table-column prop="type" label="Type"></el-table-column>
+        </el-table>
+      </el-dialog> -->
+      <!-- Add a new modal component -->
+
+      <!-- <el-dialog class="inside-dialog" v-model="detailsDialogVisible" title="Details">
+        <div class="button-container">
+          <el-icon :size="20" color="green" size="mini" @click="handleAdd(mobileView)" :el-icon-plus="Add"
+            v-hasPermi="['system:user:add']" v-if="mobileView.type === 0">
+            <Plus />
+          </el-icon>
+          <el-icon :size="20" color="blue" @click="handleUpdate(mobileView)" v-hasPermi="['system:user:edit']">
+            <Edit />
+          </el-icon>
+          <el-icon :size="20" color="red" :el-icon-plus="Delete" @click="handle_SideDelete(mobileView)"
+            v-hasPermi="['system:user:remove']">
+            <Delete />
+          </el-icon>
+        </div>
+        <div v-if="mobileView" class="details-container">
+          <div class="details-row">
+            <div class="details-label">Name:</div>
+            <div class="details-value">{{ mobileView.name }}</div>
+          </div>
+          <div class="details-row">
+            <div class="details-label">Note:</div>
+            <div class="details-value">{{ mobileView.remark }}</div>
+          </div>
+          <div class="details-row">
+            <div class="details-label">Type:</div>
+            <div class="details-value">
+              <el-tag :type="mobileView.type === 0 ? 'success' : mobileView.type === 1 ? 'Silver' : 'warning'">
+                {{ mobileView.type === 0 ? 'Directory' : mobileView.type === 1 ? 'File' : 'Link' }}
+              </el-tag>
+            </div>
+          </div>
+          <div class="details-row">
+            <div class="details-label">status:</div>
+            <div class="details-value">
+              <el-tag :type="mobileView.status === 0 ? 'success' : mobileView.status === 1 ? 'danger' : 'warning'">
+                {{ mobileView.status === 0 ? 'Enabled' : mobileView.status === 1 ? 'Disabled' : 'Sleep' }}
+              </el-tag>
+            </div>
+          </div>
+        </div>
+      </el-dialog> -->
       <!-- <s>ADD, EDIT</s> -->
       <addoredit ref="form" style="width:40%" :rules="fields_rules" :open="open" :mode="mode" :title="title" :init="mode === 'add' ?
         initialValuesAdd : initialValuesEdit" :fields="Add_Edit" @close="closeAddEdit" @submit="onSubmit"
@@ -73,6 +179,7 @@
   
   
 <script >
+// src="//unpkg.com/@element-plus/icons-vue"
 import tableHeader from "@/views/components/headerAndfooter/tableHeader"
 import addoredit from "@/views/components/addoredit/index.vue"
 import {
@@ -99,14 +206,26 @@ export default {
   },
   data() {
     return {
+      detailsDialogVisible: false,
+      mobileView: null,
+      items: [
+        { key: 'name', label: 'Name' },
+        { key: 'type', label: 'Type' },
+        { key: 'orderNum', label: 'Order' },
+        { key: 'component', label: 'Component' },
+        { key: 'remark', label: 'Note' },
+        { key: 'createBy', label: 'ADD By' },
+
+        // Add more items as needed
+      ],
       selectedRows: [],
       mode: 'add',
       loading: true,
       showSearch: true,
       switching: undefined,
-      initialValuesEdit:undefined ,
+      initialValuesEdit: undefined,
       initialValuesAdd: undefined,
-      isExpandAll: true,
+      isExpandAll: false,
       refreshTable: true,
       loading: false,
       displaySearch: true,
@@ -115,10 +234,10 @@ export default {
       title: "", // Default title for the dialog
       menuList: [],
       types: [{ label: 'Directory', value: 0 }, { label: 'menu', value: 1 }, { label: 'Button', value: 2 }],
-      total: 0,
-      paginationLayout: 'prev, pager, next', // Customize the layout based on your needs
-      isHasNextPage: false,
-      isHasPreviousPage: false,
+      // total: 0,
+      // paginationLayout: 'prev, pager, next', // Customize the layout based on your needs
+      // isHasNextPage: false,
+      // isHasPreviousPage: false,
       queryParams: {
         name: undefined,
         status: '',
@@ -132,14 +251,9 @@ export default {
         pageSize: 0
       },
 
+
       Add_Edit:
         [
-          // {
-          //   "inputtype": "dynamicFeild", 
-          //   data: {
-          //     Dept: undefined, userIds: undefined
-          //   }, inputtype: "dynamicFeild", name: "scopping", placeholder: "Enter the permision remark", span: "col-6"
-          // },
           {
             "type": "text",
             inputtype: "text",
@@ -162,7 +276,7 @@ export default {
             name: "type",
             label: "Enter the Type ",
             placeholder: "Select the Type ",
-            data: [{ label: 'Directory', value: 0 }, { label: 'menu', value: 1 }, { label: 'Link', value: 2 }],
+            data: [{ label: 'Directory', value: 0 }, { label: 'File', value: 1 }, { label: 'Link', value: 2 }],
             span: 24,
             multiple: false
           },
@@ -304,6 +418,11 @@ export default {
   //**************Methods Control*********************************************** */
 
   methods: {
+    openDetails(row) {
+      this.mobileView = row;
+      this.detailsDialogVisible = true;
+    },
+
     //Watch the form input
     emitChange(x) {
       // this.$emit('fieldChanged', this.formData);
@@ -369,6 +488,7 @@ export default {
       this.queryParams.roleId = ''
       this.queryParams.type = ''
       this.queryParams.userId = ''
+      this.queryParams.jobId = ''
       this.handleQuery();
       this.getList();
     },
@@ -428,7 +548,7 @@ export default {
             'delFlag': 0,
             'status': 1,
             'type': 2,
-            'parentId':0
+            'parentId': 0
           }
         }
 
@@ -490,9 +610,9 @@ export default {
       this.open = true;
       console.log(row.parentId)
       if (row.parentId === undefined) {
-      console.log(" I am here")
-      this.initialValuesAdd = { "delFlag": "0", 'type': 0,"parentId": 0}
-      // this.generateForm(this.switching)
+        console.log(" I am here")
+        this.initialValuesAdd = { "delFlag": "0", 'type': 0, "parentId": 0 }
+        // this.generateForm(this.switching)
       }
 
       if (row.parentId !== undefined) {
@@ -506,7 +626,7 @@ export default {
     handleUpdate(row) {
       this.mode = "Edit"
       this.open = true
-      
+
       this.initialValuesEdit = row
       // this.generateForm(this.switching)
       this.open = true
@@ -612,18 +732,115 @@ export default {
 
     },
   },
-  // watch: {
-  //   switching(newVal, oldVal) {
-  //     if (this.mode === 'add' && this.open === true) {
-  //       this.handleAdd(); // Call anotherMethod whenever `this.switching` changes
-  //     }
-  //     if (this.mode === 'edit' && this.open === true) {
-  //       this.handleUpdate(); // Call anotherMethod whenever `this.switching` changes
-  //     }
-  //   },
-  // },
 };
 </script>
+
+<style>
+.details-layout {
+  display: flex;
+  flex-direction: column;
+}
+
+.detail-item {
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 10px;
+}
+
+.detail-label {
+  font-weight: bold;
+}
+
+.detail-value {
+  margin-left: 10px;
+}
+
+/* .no-wrap {
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+} */
+
+.tableRow {
+  display: flex;
+}
+
+.tableCell {
+  flex: 1;
+  padding: 10px;
+  border-bottom: 1px solid #ccc;
+}
+
+.tableHeader {
+  font-weight: bold;
+}
+
+.tableData {
+  word-wrap: break-word;
+}
+
+.button-container {
+  display: flex;
+  justify-content: center;
+  /*This will space the buttons evenly*/
+  word-spacing: normal;
+  align-items: top;
+  /* Aligns buttons at the top of the container */
+  gap: 20px;
+}
+
+.actions_buttons {
+  display: flex;
+  word-spacing: normal;
+  align-items: top;
+  justify-content: center;
+  /* Centers content horizontally */
+  align-items: center;
+  /* Centers content vertically */
+  /* Aligns buttons at the top of the container */
+  gap: 4px;
+}
+
+.inside-dialog {
+  width: 90%;
+  max-width: 700px;
+  justify-content: center;
+  align-items: center;
+  border-radius: 25px;
+  /* margin-bottom: 70%; */
+
+}
+
+.details-container {
+  display: flex;
+  flex-direction: column;
+  padding: 10px;
+}
+
+.details-row {
+  display: flex;
+  /* Aligns items in a row */
+  justify-content: space-between;
+  /* Spreads the label and value */
+  align-items: center;
+  /* Vertically centers the content */
+  padding: 10px;
+  border-bottom: 1px solid #776e6e;
+}
+
+.details-label {
+  font-weight: bold;
+  margin-right: 10px;
+  /* Space between label and value */
+}
+
+.details-value {
+  flex-grow: 1;
+  /* Allows the value to take the remaining space */
+  text-align: center;
+  word-wrap: break-word;
+}
+</style>
   
   
   
